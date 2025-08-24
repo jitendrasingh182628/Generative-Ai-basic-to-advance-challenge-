7551d4f567e7fe7bda0db22f1e643b9c604ab0ac
jitendra singh <jitendrasingh182628@gmail.com>
2025-08-24 15:44:32 +0530

    Add files via upload

---
+++ lession1.py
@@ -0,0 +1,45 @@
+# Variable = naam + value
+a= 2 
+d= 3 
+name = "jitendra"
+print(a,d,name)
+
+# Types: int, float, str, bool
+print (type(23))
+print(type("jitendra"))
+print(type(648.5))
+print(type(True))
+
+# 👉 Precedence: () > ** > * / // % > + -
+a= 23 + 23 * 2
+b= ((23+4) * 6)
+print (a, b)
+
+# Division: / vs // vs % 
+print (23 / 4)
+print(34 // 3)
+print(54 % 5)
+
+# Float influence
+print(6 / 3.0 )
+
+# Strings & Concatenation
+first = "Hello"
+last = "world"
+final = first + " " + last + "!"
+print(final)
+
+# f-strings (clean printing)
+name = "jitendra"
+score = 23242
+print ( f"hi {name}, your score card id { score}")
+
+# Input
+user = input( " your name :")
+print(f" welcome {user} !")
+
+# Small Program
+# a = 4 
+# b = 5
+# # print (f"a+b={a+b}, a-b={a-b}, a*b={a*b}, a/b={a/b}, a//b={a//b}, a%b={a%b}")
+
