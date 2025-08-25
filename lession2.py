716a97efaa3cfe594260c023ff715697edb9bd3f
jitendra singh <jitendrasingh182628@gmail.com>
2025-08-25 07:18:53 +0530

    Add files via upload

---
+++ lession2.py
@@ -0,0 +1,8 @@
+# listing
+fruits = ["apple", "banana", "mango", "grapes", "orange", "kiwi"]
+
+print(fruits[1]) #banana
+print(fruits[-2]) #orange
+print(fruits[2:5]) # mango grapes orange
+print(fruits[:3]) # apple banana mango 
+print(fruits[3:]) #grapes orange kiwi
